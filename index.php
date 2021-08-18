b60107cac75601cef2568706d79a7ea9c95f01fd
sthasachet8 <89158169+sthasachet8@users.noreply.github.com>
2021-08-19 00:43:37 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once("teamproject.html");
+include_once("aboutus.html");
+include_once("sigin.html");
+include_once("reviews.html");
+include_once("login.html");
+include_once("contactus.html");
+
+
+?>
